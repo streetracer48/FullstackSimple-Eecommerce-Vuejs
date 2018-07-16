5f4d9597db5a07de20c2748455f87212b3bd7b0d
wasifmiraz <wasifmiraz@mail.com>
2018-07-16 21:22:17 +0600

    api up

---
+++ src/components/pages/api.php
@@ -0,0 +1,119 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "","vuephpcrud");
+if($conn->connect_error){
+	die("Could not connect to database!");
+}
+
+$res = array('error' => false);
+
+$action = 'read';
+
+if(isset($_GET['action'])){
+	$action = $_GET['action'];
+}
+
+
+if($action == 'read'){
+	$result = $conn->query("SELECT * FROM `users`");
+	$users = array();
+
+	while($row = $result->fetch_assoc()){
+		array_push($users, $row);
+	}
+
+	$res['users'] = $users;
+}
+
+if($action == 'create'){
+
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$mobile = $_POST['mobile'];
+
+	$result = $conn->query("INSERT INTO `users` (`username`, `email`, `mobile`) VALUES ('$username', '$email', '$mobile') ");
+
+	if($result){
+		$res['message'] = "User added successfully";
+	} else{
+		$res['error'] = true;
+		$res['message'] = "Could not insert user";
+	}
+}
+
+
+
+if($action == 'update'){
+	$id = $_POST['id'];
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$mobile = $_POST['mobile'];
+
+	$result = $conn->query("UPDATE `users` SET `username` = '$username', `email` = '$email', `mobile` = '$mobile' WHERE `id` = '$id'");
+
+	if($result){
+		$res['message'] = "User updated successfully";
+	} else{
+		$res['error'] = true;
+		$res['message'] = "Could not update user";
+	}
+
+}
+
+if($action == 'delete'){
+
+	$id = $_POST['id'];
+	$result = $conn->query("DELETE FROM `users` WHERE `id` = '$id'");
+
+	if($result){
+		$res['message'] = "User deleted successfully";
+	} else{
+		$res['error'] = true;
+		$res['message'] = "Could not delete user";
+	}
+
+
+}
+
+if($action == 'login'){
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$result = $conn->query("INSERT INTO `login` (`username`, `password`) VALUES ('$username', '$password') ");
+
+	if($result){
+		$res['message'] = "User added successfully";
+	} else{
+		$res['error'] = true;
+		$res['message'] = "Could not insert user";
+	}
+}
+
+
+//supplier part
+
+if($action == 'crSupplier'){
+
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$mobile = $_POST['mobile'];
+
+	$result = $conn->query("INSERT INTO `users` (`username`, `email`, `mobile`) VALUES ('$username', '$email', '$mobile') ");
+
+	if($result){
+		$res['message'] = "User added successfully";
+	} else{
+		$res['error'] = true;
+		$res['message'] = "Could not insert user";
+	}
+}
+
+
+
+
+$conn->close();
+
+header("Content-type: application/json");
+echo json_encode($res);
+die();
