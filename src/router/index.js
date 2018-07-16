fc4598bdee4c1bfc21a9bcb0487224e091f9796a
wasifmiraz <wasifmiraz@mail.com>
2018-07-16 21:07:05 +0600

    router up

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/pages/Login'
+
+
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      redirect: {path:"login"}
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+
+    
+
+      ],
+
+
+    },
+
+
+  ]
+})
